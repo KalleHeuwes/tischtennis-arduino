31b6ccb1f0c5cd34228151eb402fbe4ad23d7bc9
KalleHeuwes <karlheinz.heuwes@gmail.com>
2026-01-01 19:28:09 +0100

    Zwischenstand

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # tischtennis-arduino
 Ich habe einen Arduino Nano 33 BLE Sense Rev 2. Diesen habe ich in einen Tischtennisschläger eingebaut.
+
+## Sketches
+- data-collection: Zum Sammeln von Trainingsdaten in Kombination mit web app data-collection.html
+- smart-table-tennis-main:
+- tt_performance: Sammelt die Daten zu Beschleunigung, Drehung und Magnetometer; in Kombination mit web app tt-web
---
+++ sketches/tt_performance/tt_performance.ino
@@ -0,0 +1,89 @@
+#include <ArduinoBLE.h>
+#include <Arduino_BMI270_BMM150.h>
+
+// BLE Definitionen
+BLEService sensorService("1101"); 
+BLEStringCharacteristic accelDataChar("2101", BLERead | BLENotify, 50);
+
+const float G_TO_MS2 = 9.80665f;
+const float thresholda = 0.2;
+const float thresholdg = 2;
+
+float ax_a, ay_a, az_a;
+float mx, my, mz;
+float gx, gy, gz;
+float lastXa = 0, lastYa = 0, lastZa = 0;
+float lastXg = 0, lastYg = 0, lastZg = 0;
+
+void setup() {
+  Serial.println("setup");
+  Serial.begin(9600);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  if (!IMU.begin()) {
+    Serial.println("Fehler: IMU nicht gefunden!");
+    while (1);
+  }
+
+  if (!BLE.begin()) {
+    Serial.println("Fehler: Bluetooth konnte nicht gestartet werden!");
+    while (1);
+  }
+
+  // BLE Konfiguration
+  BLE.setLocalName("Nano33_IMU");
+  BLE.setAdvertisedService(sensorService);
+  sensorService.addCharacteristic(accelDataChar);
+  BLE.addService(sensorService);
+  
+  // Startet das Sichtbar-Sein
+  BLE.advertise();
+  Serial.println("Bluetooth gestartet. Warte auf Verbindung...");
+}
+
+void loop() {
+  // Überprüfe ständig auf eine Verbindung
+  BLEDevice central = BLE.central();
+
+  if (central) {
+    digitalWrite(LED_BUILTIN, HIGH); // LED an bei Verbindung
+    Serial.print("Verbunden mit: ");
+    Serial.println(central.address());
+
+    while (central.connected()) {
+      if (IMU.accelerationAvailable()) {
+        IMU.readAcceleration(ax_a, ay_a, az_a);
+        float ax = ax_a * G_TO_MS2;
+        float ay = ay_a * G_TO_MS2;
+        float az = az_a * G_TO_MS2;
+
+        IMU.readGyroscope(gx, gy, gz);
+
+        IMU.readMagneticField(mx, my, mz);
+
+        if (abs(ax - lastXa) > thresholda || abs(ay - lastYa) > thresholda || abs(az - lastZa) > thresholda ||
+            abs(gx - lastXg) > thresholdg || abs(gy - lastYg) > thresholdg || abs(gz - lastZg) > thresholdg
+            ) {          
+          String data = String(ax) + "," + String(ay) + "," + String(az) + "," +
+                        String(gx) + "," + String(gy) + "," + String(gz) + "," +
+                        String(mx) + "," + String(my) + "," + String(mz);   // Daten als CSV-String formatieren
+          accelDataChar.writeValue(data);                                   // Per Bluetooth senden          
+          Serial.println("Gesendet: " + data);
+          lastXa = ax; lastYa = ay; lastZa = az;
+          lastXg = gx; lastYg = gy; lastZg = gz;
+        }
+      }
+      // Kurze Pause zur Entlastung des BLE-Stacks
+      delay(20);
+    }   
+
+    // Wenn die Verbindung getrennt wurde
+    digitalWrite(LED_BUILTIN, LOW); // LED aus
+    Serial.println("Verbindung getrennt.");
+    
+    // WICHTIG: Explizit wieder sichtbar werden
+    BLE.advertise(); 
+    Serial.println("Wieder im Advertising-Modus...");
+  }
+}
